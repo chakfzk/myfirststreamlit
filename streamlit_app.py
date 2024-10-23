ae1e3aaa1dd1e85dd35b2ea138d38e216c26a121
chakfzk <yngjn0@gmail.com>
2024-10-23 10:03:10 +0000

    앱 만들기 시작

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-st.title("👋🏻 Streamlit 앱 만들기")
-st.subheader("이 페이지는 실습 페이지입니다.")
-st.write("오른쪽 위의 'fork' 버튼을 눌러주세요. 이 페이지와 앱이 그대로 복사됩니다.")
+st.title("👋🏻 연수 실습 페이지")
+st.subheader("이 페이지는 영준쌤이 만들었습니다.")
+st.info("10월 23일 수요일에 연수를 들으며 만들고 있습니다. 혼자 열심히 해보다가 놓치지 않아보려고 합니다. giphy에서 gif파일을 가져왔고, video를 이용해서 유튜브를 임베딩하고 있습니다.")
+st.link_button("영준쌤의 과학", "https://www.notion.so/yngjn0/92e1ac1c012040989ac07efae302c53b")
 
-st.link_button("streamlit 매뉴얼 페이지 바로가기!", "https://surish.notion.site/streamlit-113eef51495c8083986cc65f2d07470c?pvs=73")
-
-# st.success("초록색 창")
-# st.error("빨간색 창")
+st.success("초록색 창")
+st.error("빨간색 창")
 st.info("파란색 창")
-# st.warning("노란색 창") # ctrl+/ : 주석처리
-st.image("https://media.giphy.com/media/8bE0EERrvXkq5S9BCa/giphy.gif?cid=ecf05e479tvvde0o3d3do3g67yoq0jn4zzyg9hyt63stuf0x&ep=v1_gifs_related&rid=giphy.gif&ct=g", caption="Welcome to coding world") 
+st.warning("노란색 창") # ctrl+/ : 주석처리
+st.image("https://media.giphy.com/media/lGHFhGuIUX6tW/giphy.gif?cid=ecf05e47nxamoc2hpphvhgqo4tcqqy8hbcvzhntmc7jbs9y0&ep=v1_gifs_search&rid=giphy.gif&ct=g","funny free kick") 
+st.video("https://youtu.be/xdJ3RP92UlU?si=KOPc4H6QA8MjzJT3")
+st.image("data/예비군 교육훈련 소집통지서(김영준).jpg")
