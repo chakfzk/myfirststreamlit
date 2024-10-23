547770c1c74b0a50e7530b79c834bdf1ec914a14
chakfzk <yngjn0@gmail.com>
2024-10-23 10:21:27 +0000

    24삭제

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("👋🏻 연수 실습 페이지 241023")
+st.title("👋🏻 연수 실습 페이지 1023")
 st.subheader("이 페이지는 영준쌤이 만들었습니다.")
 st.info("10월 23일 수요일에 연수를 들으며 만들고 있습니다. 혼자 열심히 해보다가 놓치지 않아보려고 합니다. giphy에서 gif파일을 가져왔고, video를 이용해서 유튜브를 임베딩하고 있습니다. 내가 가지고 있는 파일을 data 넣고, 상대 경로 복사를 해서 불러와 봤습니다.")
 st.link_button("영준쌤의 과학", "https://www.notion.so/yngjn0/92e1ac1c012040989ac07efae302c53b")
